1b131be5b6dfaacf6ea5e1d02e062d71c41252a9
Faizan Anwar <faizana1298@gmail.com>
2024-12-03 23:39:04 +0530

    add admin/dashboard controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root 'calculations#index'
   resources :calculations, only: [:create]
+  namespace :admin do
+    get 'dashboard', to: 'dashboard#index'
+  end
 end
